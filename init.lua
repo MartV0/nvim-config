bf538009b9cab04da40ea4eccda23fdccf701af2
martijnv <martijnvoordouw@gmail.com>
2023-08-31 12:58:57 +0200

    formatted

--- init.lua
+++ init.lua
@@ -1,36 +1,36 @@
 ----------------------------
 -------OPTIONS--------------
 ----------------------------
-vim.opt.relativenumber=true
-vim.opt.number=true
-vim.opt.cursorline=true
-vim.opt.splitbelow=true
-vim.opt.splitright=true
-vim.opt.ignorecase=true
-vim.opt.smartcase=true
-vim.opt.wrap=false
-vim.opt.hlsearch=false
-vim.opt.history=2000
-vim.opt.scrolloff=10
-vim.opt.hidden=true
-vim.opt.ttimeoutlen=150
-vim.opt.sidescrolloff=10
-vim.opt.sidescroll=6
-vim.opt.startofline=true
-vim.opt.linebreak=true
-vim.opt.backup=true
-vim.opt.colorcolumn='80'
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.cursorline = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.wrap = false
+vim.opt.hlsearch = false
+vim.opt.history = 2000
+vim.opt.scrolloff = 10
+vim.opt.hidden = true
+vim.opt.ttimeoutlen = 150
+vim.opt.sidescrolloff = 10
+vim.opt.sidescroll = 6
+vim.opt.startofline = true
+vim.opt.linebreak = true
+vim.opt.backup = true
+vim.opt.colorcolumn = '80'
 --folding with treesitter:
-vim.opt.foldmethod='expr'
-vim.opt.foldexpr='nvim_treesitter#foldexpr()'
-vim.opt.foldenable=false
-vim.opt.foldnestmax=3
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldenable = false
+vim.opt.foldnestmax = 3
 --tabs are 4 spaces
 --TODO mischien vervangen met verschillende opties per taal
-vim.opt.tabstop=4
-vim.opt.softtabstop=4
-vim.opt.shiftwidth=4
-vim.opt.expandtab=true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 -- mapping related stuff
 vim.g.mapleader = ' '
 vim.g.NERDCreateDefaultMappings = 0
@@ -40,14 +40,14 @@ vim.g.NERDCreateDefaultMappings = 0
 ----------------------------
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -56,31 +56,32 @@ local plugins = {
     {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
-        config = function ()
-          local configs = require("nvim-treesitter.configs")
-          configs.setup({
-              -- folding zooi staat bij options
-              sync_install = false,
-              highlight = { enable = true },
-              indent = { enable = true },
-              ensure_installed = {"lua", "javascript", "python", "haskell", "c_sharp"},
+        config = function()
+            local configs = require("nvim-treesitter.configs")
+            configs.setup({
+                -- folding zooi staat bij options
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+                ensure_installed = { "lua", "javascript", "python", "haskell", "c_sharp" },
             })
         end
     },
     { "bluz71/vim-nightfly-colors", name = "nightfly", lazy = false, priority = 1000 },
     {
-      "nvim-tree/nvim-tree.lua",
-      version = "*",
-      lazy = false,
-      dependencies = {
-        "nvim-tree/nvim-web-devicons",
-      },
-      config = function()
-        require("nvim-tree").setup {}
-      end,
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end,
     },
     {
-        'nvim-telescope/telescope.nvim', branch = '0.1.x',
+        'nvim-telescope/telescope.nvim',
+        branch = '0.1.x',
         dependencies = {
             'nvim-lua/plenary.nvim',
             'BurntSushi/ripgrep',
@@ -105,22 +106,22 @@ local plugins = {
     {
         "NMAC427/guess-indent.nvim",
         config = function()
-          require("guess-indent").setup {}
+            require("guess-indent").setup {}
         end,
     },
     {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim", 'BurntSushi/ripgrep',
-            "folke/trouble.nvim", 'nvim-telescope/telescope.nvim'},
+            "folke/trouble.nvim", 'nvim-telescope/telescope.nvim' },
         opts = {
-            highlight = { pattern = [[.*<(KEYWORDS)\s*]]},
+            highlight = { pattern = [[.*<(KEYWORDS)\s*]] },
             search = { pattern = [[\b(KEYWORDS)\b]] }
-	    }
+        }
     },
     {
-     "folke/trouble.nvim",
-     dependencies = { "nvim-tree/nvim-web-devicons" },
-     opts = {},
+        "folke/trouble.nvim",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        opts = {},
     },
     {
         "iamcco/markdown-preview.nvim",
@@ -132,49 +133,49 @@ local plugins = {
     {
         'simrat39/symbols-outline.nvim',
         config = function()
-          require("symbols-outline").setup()
+            require("symbols-outline").setup()
         end,
     },
     {
         'karb94/neoscroll.nvim',
         config = function()
-          require("neoscroll").setup()
+            require("neoscroll").setup()
         end,
     },
     {
-       "m4xshen/hardtime.nvim",
-       dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
-       opts = {
+        "m4xshen/hardtime.nvim",
+        dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+        opts = {
             restriction_mode = "hint",
             disable_mouse = false,
         }
     },
-    {'VonHeikemen/lsp-zero.nvim', branch = 'dev-v3'},
+    { 'VonHeikemen/lsp-zero.nvim',        branch = 'dev-v3' },
     --- Uncomment these if you want to manage LSP servers from neovim
-    {'williamboman/mason.nvim'},
-    {'williamboman/mason-lspconfig.nvim'},
+    { 'williamboman/mason.nvim' },
+    { 'williamboman/mason-lspconfig.nvim' },
     -- LSP Support
     {
         'neovim/nvim-lspconfig',
         dependencies = {
-          {'hrsh7th/cmp-nvim-lsp'},
+            { 'hrsh7th/cmp-nvim-lsp' },
         },
     },
     -- Autocompletion
     {
         'hrsh7th/nvim-cmp',
         dependencies = {
-          {'L3MON4D3/LuaSnip'},
+            { 'L3MON4D3/LuaSnip' },
         }
     },
     {
-      "folke/which-key.nvim",
-      event = "VeryLazy",
-      init = function()
-        vim.o.timeout = true
-        vim.o.timeoutlen = 300
-      end,
-      opts = {}
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {}
     },
 }
 require("lazy").setup(plugins)
@@ -186,11 +187,11 @@ local lsp = require('lsp-zero').preset({})
 lsp.on_attach(function(client, bufnr)
     -- see :help lsp-zero-keybindings
     -- to learn the available actions
-    lsp.default_keymaps({buffer = bufnr, preserve_mappings = false})
-    local opts = {buffer = bufnr}
+    lsp.default_keymaps({ buffer = bufnr, preserve_mappings = false })
+    local opts = { buffer = bufnr }
 
-    vim.keymap.set({'n', 'x'}, 'gq', function()
-        vim.lsp.buf.format({async = false, timeout_ms = 10000})
+    vim.keymap.set({ 'n', 'x' }, 'gq', function()
+        vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
     end, opts)
 end)
 
@@ -200,36 +201,36 @@ local cmp = require('cmp')
 local cmp_action = require('lsp-zero').cmp_action()
 
 cmp.setup({
-  mapping = {
-    ['<CR>'] = cmp.mapping.confirm({select = false}),
-    ['<Tab>'] = cmp_action.tab_complete(),
-    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-  }
+    mapping = {
+        ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        ['<Tab>'] = cmp_action.tab_complete(),
+        ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+    }
 })
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  -- Replace the language servers listed here
-  -- with the ones you want to install
-  ensure_installed = {"lua_ls", "jedi_language_server"},
-  handlers = {
-    lsp.default_setup,
-    lua_ls = function()
-      -- (Optional) Configure lua language server for neovim
-      require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-    end,
-  },
+    -- Replace the language servers listed here
+    -- with the ones you want to install
+    ensure_installed = { "lua_ls", "jedi_language_server" },
+    handlers = {
+        lsp.default_setup,
+        lua_ls = function()
+            -- (Optional) Configure lua language server for neovim
+            require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+        end,
+    },
 })
 --TODO
 --DAP
 --formatter
 --snippets
 --maybe add later stuff:
-    --easy align maybe
-    --telescope extensie -> zoek door text
-    --harpoon
-    --kijken voor coole motions plugins
-    --iets voor git -> git-conflicts.nvim
+--easy align maybe
+--telescope extensie -> zoek door text
+--harpoon
+--kijken voor coole motions plugins
+--iets voor git -> git-conflicts.nvim
 vim.cmd [[colorscheme nightfly]]
 require('lualine').setup()
 ----------------------------
@@ -238,8 +239,8 @@ require('lualine').setup()
 -- leader key defined above under options
 -- define common options
 local opts = {
-    noremap = true,      -- non-recursive
-    silent = true,       -- do not show message
+    noremap = true, -- non-recursive
+    silent = true,  -- do not show message
 }
 -- automatically reselect after indenting
 vim.keymap.set('v', '<', '<gv', opts)
@@ -257,4 +258,4 @@ vim.keymap.set('n', '<leader>g', builtin.git_files, opts)
 vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle, opts)
 vim.keymap.set("n", "]t", require("todo-comments").jump_next)
 vim.keymap.set("n", "]t", require("todo-comments").jump_prev)
-vim.keymap.set({'n','v'}, '<leader>c', '<plug>NERDCommenterToggle', opts)
+vim.keymap.set({ 'n', 'v' }, '<leader>c', '<plug>NERDCommenterToggle', opts)
